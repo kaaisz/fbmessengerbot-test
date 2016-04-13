1e0fc9dfd89b59106f15b59f48c1cd8702e13755
tmtysk <tmtysk@gmail.com>
2016-04-13 18:28:20 +0900

    first import

---
+++ README.md
@@ -0,0 +1,21 @@
+# fbmessengerbot-test
+
+## これはなに？
+
+Facebook Messenger Bot をひとまず PHP on Heroku で動かしてみるキットです.
+silex を使っています.
+
+## 始め方
+
+* Facebook App と Facebook Page を作成してください.
+* Heroku アカウントを取得してください.
+* 下の Deploy ボタンを押してデプロイします. 環境変数 FACEBOOK_PAGE_VERIFY_TOKEN に適当な文字列を入れておきます(後で使います). FACEBOOK_PAGE_ACCESS_TOKEN は空欄としておきます.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](http://bit.ly/fbmessengerbot-test-deploy)
+
+* https://developers.facebook.com/docs/messenger-platform/quickstart を参考に、以下の手順を進めてください.
+    * Facebook App に Messenger を利用する設定を追加し、Facebook Page の Access Token を取得 -> Heroku にデプロイしたアプリケーションの環境変数 FACEBOOK_PAGE_ACCESS_TOKEN に設定します.
+    * Webhooks URL として、 <Heroku にデプロイしたURL>/callback を指定し、Verify Token にはデプロイ時に指定した FACEBOOK_PAGE_VERIFY_TOKEN を指定します. events として messages にチェックを入れ、Verify します.
+    * Facebook 側の所定の URL に対して POST リクエストを送信し、Facebook Page に App を登録(Subscribe)します.
+* Facebook メッセンジャーから Facebook Page に話しかけてみましょう.
+* web/index.php を編集して Hack してみましょう. Enjoy!
---
+++ web/index.php
@@ -0,0 +1,60 @@
+<?php
+
+require('../vendor/autoload.php');
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+
+$app->before(function (Request $request) use($bot) {
+    // TODO validation
+});
+
+$app->get('/callback', function (Request $request) use ($app) {
+    $response = "";
+    if ($request->query->get('hub_verify_token') === getenv('FACEBOOK_PAGE_VERIFY_TOKEN')) {
+        $response = $request->query->get('hub_challenge');
+    }
+
+    return $response;
+});
+
+$app->post('/callback', function (Request $request) use ($app) {
+    // Let's hack from here!
+    $body = json_decode($request->getContent(), true);
+    $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/']);
+
+    foreach ($body['entry'] as $obj) {
+        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
+
+        foreach ($obj['messaging'] as $m) {
+            $app['monolog']->addInfo(sprintf('messaging: %s', json_encode($m)));
+            $from = $m['sender']['id'];
+            $text = $m['message']['text'];
+
+            if ($text) {
+                $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
+                $json = [
+                    'recipient' => [
+                        'id' => $from, 
+                    ],
+                    'message' => [
+                        'text' => sprintf('%sじゃない', $text), 
+                    ],
+                ];
+                $client->request('POST', $path, ['json' => $json]);
+            }
+        }
+
+    }
+
+    return 0;
+});
+
+$app->run();
